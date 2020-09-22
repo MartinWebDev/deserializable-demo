84c8a0170873ea91fa1e31b996843438609150b3
MartinWebDev <martin@martinwebdev.co.uk>
2020-09-22 12:28:17 +1000

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: "skills", loadChildren: () => import("./modules/skills/skills.module").then(x => x.SkillsModule) },
-  { path: "", redirectTo: "/start", pathMatch: "full" },
-  { path: "**", redirectTo: "/start", pathMatch: "full" }
+    { path: "skills", loadChildren: () => import("./modules/skills/skills.module").then((x) => x.SkillsModule) },
+    { path: "", redirectTo: "/start", pathMatch: "full" },
+    { path: "**", redirectTo: "/start", pathMatch: "full" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
-// import { SkillsModule } from "./modules/skills/skills.module";
 import { HomeModule } from "./modules/home/home.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    // SkillsModule,
-    HomeModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, HomeModule, AppRoutingModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
